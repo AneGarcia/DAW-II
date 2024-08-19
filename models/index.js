d9e954907d698c281d848050e478c839f3ee955d
AneGarcia <anelise300407@gmail.com>
2024-08-19 11:57:34 -0300

    Teste

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const config = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    config
+);
+
+const db = {};
+db.sequelize = Sequelize;
+db.sequelize = sequelize;
+db.produtos = require("./produto.model.js")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ models/loja.model.js
@@ -0,0 +1,6 @@
+module.exports = (sequelize, Sequelize) => {
+    const Loja = sequelize.define("loja",{
+        nome: {type: Sequelize.STRING},
+        endereco: {type: Sequelize.}
+    })
+}
