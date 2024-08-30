a8b1836d7afc87e885e8a5854fb2e96c26766818
AneGarcia <anelise300407@gmail.com>
2024-08-30 11:48:08 -0300

    Rotas e controllers

--- controllers/produto.controllers.js
+++ controllers/produto.controllers.js
@@ -30,7 +30,7 @@ exports.create = (req, res) => {
 
 exports.findAll = (req, res) => {
     const nome = req.query.nome;
-    var condition = nome ? {nome: { [Op.iLike]: '%${nome}%'} } : null;
+    var condition = nome ? {nome: { [Op.iLike]: `%${nome}%`} } : null;
 
     Produto.findAll({where: condition})
     .then((data) => res.send(data))
@@ -49,14 +49,78 @@ exports.findOne = (req, res) => {
         if (data) {
             res.send(data);
         }else{
-            res
-                .status(404)
-                .send({message: 'Não foi possível encontrar produto com id ${id}'});
+            res.status(404).send({
+                message: `Não foi possível encontrar produto com id ${id}`,
+            });
         }
     })
     .catch((err) => 
         res.status(500).send({
-            message: err.message || 'Erro ao buscar por produto com id ${id}',
+            message: err.message || `Erro ao buscar por produto com id ${id}`,
         })
     );
+};
+
+exports.update = (req,res) => {
+    const id = req.params.id;
+
+    Produto.update(req.body, {
+        where: {id: id},
+    })
+    .then((num) => {
+        if (num == 1) {
+            res.send({
+                message: "Produto foi atualizado com sucesso.",
+            });
+        }else{
+            res.send({
+                message: `Não foi possível atualizar Produto com id= ${id}. Talvez o produto não tenha sido encontrado ou req.body está vazio`,
+            });
+        }
+    })
+    .cacth((err) => {
+        res.status(500).send({
+            message: err.message || "Erro em atualizar o Produto via id=" + id,
+        });
+    });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Produto.destroy({
+        where: {id: id},
+    })
+    .then((num) => {
+        if (num == 1){
+            res.send({
+                message: "Produto foi deletado com sucesso!",
+            });
+        }
+        else{
+            res.send({
+                message: "Não é possível deletar esse Produto. Ele não foi encontrado"
+            })
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: "Não é possível deletar Produto com id=" + id,
+        });
+    });
+};
+
+exports.deleteAll = (req, res) => {
+    Produto.destroy({
+        where: {},
+        truncate: false,
+    })
+    .then((nums) =>{
+        res.send({message: `${nums} Produtos foram deletados com sucesso!`});
+    })
+    .cacth((err) =>{
+        res.status(500).send({
+            message: err.message || "Erro enquanto deletava os produtos.",
+        });
+    });
 };
---
+++ routes/produto.routes.js
@@ -0,0 +1,14 @@
+module.exports = (app) =>{
+    const produtos = require("../controllers/produto.controllers.js");
+    var router = require("express").Router();
+
+    router.post("/", produtos.create);
+    router.get("/", produtos.findAll);
+    router.get("/:id", produtos.findOne);
+    router.put("/:id", produtos.update);
+    router.delete("/:id", produtos.delete);
+    router.delete("/", produtos.deleteAll);
+
+
+    app.use("/produtos", router);
+};
