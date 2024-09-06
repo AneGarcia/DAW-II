034930c05e2c6971b860c96a5474a1fb7f7cc71c
AneGarcia <anelise300407@gmail.com>
2024-09-06 11:45:27 -0300

    arquivos lojas

--- controllers/loja.controllers.js
+++ controllers/loja.controllers.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const db = require("../models");
 const Loja = db.lojas;
 const Op = db.sequelize.Op;
@@ -13,3 +14,69 @@ exports.create = (req,res) => {
     Loja.create(loja).then(data => res.send(data))
     .catch((err) => res.status(500).send({message: err.message || "Erro ao criar a loja."}));
 }
+
+exports.findAll = (req, res) => {
+    Loja.findAll().then(data => res.send(data))
+    .catch(err => res.status(500).send({message: err.message || "Erro ao buscar lojas"}));
+};
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Loja.findByPk(id).then(data =>{
+        if(data){
+            res.send(data);
+        }
+        else{
+            res.status(404).send({message: "Não foi possível encontrar uma loja com o id" + id});
+        }
+    }).catch(err => res
+        .status(500)
+        .send({message:err.message || "Erro ao buscar por loja"})
+    );
+};
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Loja.update(req.body, {where: {id: id}})
+    .then((num) => {
+        if(num = 1){
+            req.send({message: "Loja atualizada com sucesso"});
+        }else{
+            res.status(404)
+            .send({message: "Não foi possível atualizar. req.body vazio ou loja não encontrada."});
+        }
+    })
+    .catch((err) => {
+        res.status(500)
+        .send({message: err.message || "Erro ao atualizar loja"});
+    });
+};
+
+exports.delete = (req,res) =>{
+    const id = req.params.id;
+
+    Loja.destroy({where: {id: id}}).then((num) => {
+        if(num = 1){
+            res.send({message: "Loja remocida com sucesso"});
+        }else{
+            res.send({message: "Loja não encontrada. Id: " + id});
+        }
+    })
+    .catch((err) =>
+        res.status(500)
+        .send({message: err.message || "Erro ao deletar loja"})
+    );
+};
+
+exports.deleteAll = (req,res) => {
+    Loja.destroy({
+        where: {},
+        truncate: false,
+    }).then((nums) => {
+        res.send({message: `${nums} lojas deletadas`});
+    })
+    .catch((err) => {
+        res.status(500).send({message: err.message || "Erro ao remover loja"});
+    });
+};
